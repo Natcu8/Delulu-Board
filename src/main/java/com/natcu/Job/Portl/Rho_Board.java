36061752bf72308bcfa9e0242944e8f7485f3c69
YashwanthKumar49 <lyashwanth.98@gmail.com>
2025-03-04 01:32:11 -0700

    Commit from Leo PC

--- src/main/java/com/natcu/Job/Portl/Rho_Board.java
+++ src/main/java/com/natcu/Job/Portl/Rho_Board.java
@@ -2,12 +2,18 @@ package com.natcu.Job.Portl;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @SpringBootApplication
 public class Rho_Board {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Rho_Board.class, args);
+
 	}
 
+
 }
+
+

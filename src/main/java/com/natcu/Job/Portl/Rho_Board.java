753c14457a0972a1703193b3c708f246be441b57
Natcu8 <35478626+Natcu8@users.noreply.github.com>
2025-09-07 16:48:15 +0530

    test 4

--- src/main/java/com/natcu/Job/Portl/Rho_Board.java
+++ src/main/java/com/natcu/Job/Portl/Rho_Board.java
@@ -11,7 +11,7 @@ public class Rho_Board {
 	public static void main(String[] args) {
 		SpringApplication.run(Rho_Board.class, args);
 
-		//Testing CI Pipeline Committ
+		//Testing CI Pipeline 
 	}
 
 

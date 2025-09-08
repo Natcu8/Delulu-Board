55d19ff6a12016cc65339752257c02a96ec74e9a
Natcu8 <35478626+Natcu8@users.noreply.github.com>
2025-09-08 22:07:03 +0530

    rgrgre

--- src/main/java/com/natcu/Job/Portl/Rho_Board.java
+++ src/main/java/com/natcu/Job/Portl/Rho_Board.java
@@ -3,7 +3,6 @@ package com.natcu.Job.Portl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 
 @SpringBootApplication
 public class Rho_Board {
